07ff25c07ffc1073d4c5036ab77227b905fc3f6a
kiplagatwillan <willankpl01@gmail.com>
2025-06-17 22:25:08 +0300

    edited main package.json

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
-import express, { Request, Response, NextFunction } from 'express';
-import taskRoutes from './routes/tasks';
-
-const app = express();
-app.use(express.json());
-app.get("/", (_req: Request, res: Response) => {
-  res.send("<h1>Welcome to the Task API</h1>");
+import app from '.';
+const port = process.env.PORT || 65000;
+app.listen(port, () =>{
+  console.log(`Api running on port6 ${port}`);
 });
-app.use("/tasks", taskRoutes);
-
-app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
-  console.error(err);
-  res.status(500).json({ message: "Internal server error" });
-});
-export default app;
--- src/server.ts
+++
@@ -1,5 +0,0 @@
-import app from '.';
-const port = process.env.PORT || 65000;
-app.listen(port, () =>{
-  console.log(`Api running on port6 ${port}`);
-});
